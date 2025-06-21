0941ca325f6b828412ebc2dfb73fc08bb5e8b444
charu-analyst <charuyadav594@gmail.com>
2025-06-22 01:29:20 +0530

    implement multer to upload images

--- server/api/v1/controllers/student/studentController.js
+++ server/api/v1/controllers/student/studentController.js
@@ -4,7 +4,7 @@ const statusType = require("../../../../enum/status");
 const userType = require("../../../../enum/userType");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+const commonFunction = require("../../../../helpers/utils");
 const { studentServices } = require("../../services/studentServices");
 const status = require("../../../../enum/status");
 const {
@@ -24,41 +24,171 @@ const {
   updateUser,
   countTotalUser,
 } = userServices;
-const defPass=process.env.DEFAULT_PASSWORD
+const defPass = process.env.DEFAULT_PASSWORD;
+// exports.studentCreation1 = async (req, res, next) => {
+//   try {
+//     let { data } = req.body;
+//     const requestData = data;
+//     // const requestData = JSON.parse(data);
+//     console.log("requestData==", requestData);
+//     const files = req.files || {};
+//     // E.g.: req.files['aadharCard'] = [ fileObj ]
+//     const proof = {};
+
+//     ["aadharCard", "birthCertificate", "tc", "studentImage"].forEach((key) => {
+//       const arr = files[key];
+//       if (Array.isArray(arr) && arr[0]) {
+//         proof[key] = arr[0].path; // or URL after uploading to cloud
+//       }
+//     });
+
+//     // Check that at least one image is uploaded
+//     if (Object.keys(proof).length === 0) {
+//       return res
+//         .status(400)
+//         .json({ message: "At least one proof image is required." });
+//     }
+//     const hashedPassword = bcrypt.hashSync(defPass, 10);
+//     const admissionDateStr = new Date(requestData?.classes?.[0]?.admissionDate);
+//     const year = admissionDateStr.getFullYear();
+//     const count = await countTotalStudent({
+//       "classes.admissionDate": { $gte: `01-01-${year}`, $lte: `31-12-${year}` },
+//     });
+//     const paddedCount = String(count + 1).padStart(2, "0");
+//     requestData.admissionNumber = `ADM${year}${requestData.currentClass.section}${requestData.currentClass.rollNumber}${paddedCount}`;
+
+//     const totalUsers = await countTotalUser();
+//     requestData.userId = `nova${String(totalUsers + 1).padStart(6, "0")}`;
+
+//     requestData.password = hashedPassword;
+//     const base64Aadhar = requestData?.proof?.aadharCard?.image;
+//     const base64Birth = requestData?.proof?.birthCertificate?.image;
+//     const base64TC = requestData?.proof?.tc?.image;
+
+//     if (requestData.studentImage) {
+//       const imgurl = await commonFunction.getSecureUrl(
+//         requestData.studentImage,
+//         "studentProof"
+//       );
+//       console.log("imgurl==", imgurl);
+
+//       requestData.studentImage = imgurl;
+//     }
+//     if (base64Aadhar) {
+//       const imgurl = await commonFunction.getSecureUrl(
+//         base64Aadhar,
+//         "studentProof"
+//       );
+//       console.log("adhar", imgurl);
+
+//       requestData.proof.aadharCard.image = imgurl;
+//     }
+//     if (base64Birth) {
+//       const imgurl = await commonFunction.getSecureUrl(
+//         base64Birth,
+//         "studentProof"
+//       );
+//       console.log("birth==", imgurl);
+
+//       requestData.proof.birthCertificate.image = imgurl;
+//     }
+//     if (base64TC) {
+//       const imgurl = await commonFunction.getSecureUrl(
+//         base64TC,
+//         "studentProof"
+//       );
+
+//       requestData.proof.tc.image = imgurl;
+//     }
+//     console.log(requestData.proof, "=========");
+
+//     console.log(JSON.stringify(requestData.proof, null, 2));
+//     const result = await createStudent(requestData);
+//     await createUser({
+//       userId: requestData.userId,
+//       password: hashedPassword,
+//       effDate: requestData.admissionDate,
+//       role: userType.STUDENT,
+//       newUserCount: totalUsers + 1,
+//     });
+//     return res.status(statusCode.OK).send({
+//       statusCode: statusCode.OK,
+//       responseMessage: responseMessage.STUDENT_ADDED,
+//       result: result,
+//     });
+//   } catch (error) {
+//     return next(error);
+//   }
+// };
 exports.studentCreation = async (req, res, next) => {
   try {
     let { data } = req.body;
-    const requestData=JSON.parse(data)
-    const hashedPassword=await bcrypt.hashSync(defPass,10);
-    const admissionDateStr = new Date(requestData?.classes?.[0]?.admissionDate);
+    let requestData =
+      typeof req.body.data === "string"
+        ? JSON.parse(req.body.data)
+        : req.body.data;
+    const files = req.files || req.file;
+    const proof = {};
+
+    ["aadharCard", "birthCertificate", "tc", "studentImage"].forEach((key) => {
+      const arr = files[key];
+      if (Array.isArray(arr) && arr[0]) {
+        proof[key] = arr[0].path;
+      }
+    });
+
+    if (Object.keys(proof).length === 0) {
+      return res
+        .status(400)
+        .json({ message: "At least one proof image is required." });
+    }
+
+    requestData.proof = proof;
+
+    const hashedPassword = bcrypt.hashSync(defPass, 10);
+    requestData.password = hashedPassword;
+
+    const admissionDateStr = new Date(requestData?.currentClass?.admissionDate);
     const year = admissionDateStr.getFullYear();
     const count = await countTotalStudent({
-      "classes.admissionDate": { $gte: `01-01-${year}`, $lte: `31-12-${year}` },
+      "classHistory.admissionDate": {
+        $gte: new Date(`${year}-01-01`),
+        $lte: new Date(`${year}-12-31`),
+      },
     });
-    const incrementedCount = count + 1;
-    const paddedCount = String(incrementedCount).padStart(2, "0");
+
+    const paddedCount = String(count + 1).padStart(2, "0");
     requestData.admissionNumber = `ADM${year}${requestData.currentClass.section}${requestData.currentClass.rollNumber}${paddedCount}`;
-       const totalUsers = await countTotalUser(); 
-    const newUserCount = totalUsers + 1;
-    const paddedUserCount = String(newUserCount).padStart(6, "0");
-    requestData.userId = `nova${paddedUserCount}`;
-    console.log("requestData====",requestData);
-    hashedPassword.password=hashedPassword;
-    console.log("requestData==",requestData)
+
+    const totalUsers = await countTotalUser();
+    requestData.userId = `nova${String(totalUsers + 1).padStart(6, "0")}`;
+
+    for (const key of Object.keys(proof)) {
+      const url = await commonFunction.getSecureUrl(
+        proof[key],
+        "studentProofs"
+      );
+      requestData.proof[key] = { image: url };
+    }
 
     const result = await createStudent(requestData);
+    await createUser({
+      userId: requestData.userId,
+      password: hashedPassword,
+      effDate: requestData.admissionDate,
+      role: userType.STUDENT,
+      newUserCount: totalUsers + 1,
+    });
 
-    await createUser({userId:requestData.userId,password:hashedPassword,effDate:requestData.admissionDate,role:userType.STUDENT,newUserCount})
     return res.status(statusCode.OK).send({
       statusCode: statusCode.OK,
       responseMessage: responseMessage.STUDENT_ADDED,
-      result: result,
+      result,
     });
   } catch (error) {
     return next(error);
   }
 };
-
 exports.getAllStudent = async (req, res, next) => {
   try {
     const result = await findStudentData({ status: statusType.ACTIVE });
@@ -78,37 +208,30 @@ exports.getAllStudent = async (req, res, next) => {
   }
 };
 
-exports.editStudentDetails=async  (req,res,next)=>{
+exports.editStudentDetails = async (req, res, next) => {
   try {
-    const {}=req.body;
-    const isAdminExist=await a();
-    if(!isAdminExist){
-       return res.status(statusCode.OK).send({
+    const {} = req.body;
+    const isAdminExist = await a();
+    if (!isAdminExist) {
+      return res.status(statusCode.OK).send({
         statusCode: statusCode.badRequest,
         responseMessage: responseMessage.UNAUTHORIZED,
       });
     }
-    const obj={
-
-    }
-    const updateProfile=await updateStudent({_id:stdId},{obj});
-     return res.status(statusCode.OK).send({
+    const obj = {};
+    const updateProfile = await updateStudent({ _id: stdId }, { obj });
+    return res.status(statusCode.OK).send({
       statusCode: statusCode.OK,
       responseMessage: responseMessage.UPDATE_SUCCESS,
       result: updateProfile,
     });
   } catch (error) {
-    console.log("Error while trying to edit profile",error);
+    console.log("Error while trying to edit profile", error);
     return next(error);
-    
   }
-}
+};
 
-exports.deleteSTudents=async(req,res,next)=>{
+exports.deleteSTudents = async (req, res, next) => {
   try {
-    
-  } catch (error) {
-    
-  }
-}
-
+  } catch (error) {}
+};
